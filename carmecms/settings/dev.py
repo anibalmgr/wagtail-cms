cf4c8dfb28a46b8d8537127fd83e538cea20da84
Anibal <anibal.gatica@mail.udp.cl>
2022-05-08 22:35:18 +0100

    first commit

---
+++ carmecms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1$m!4&i!jm4722e1dd7a9%v4m8ouq#_-o7ohq%#d_s#qxm#x7l'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
